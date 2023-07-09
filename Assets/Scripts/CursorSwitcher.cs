5af9ee34128f43f9886775140cf09838d40863ee
Bence Balazs <56939786+benceBalazs@users.noreply.github.com>
2023-07-09 13:17:03 +0200

    some buttons work with sound now

--- Assets/Scripts/CursorSwitcher.cs
+++ Assets/Scripts/CursorSwitcher.cs
@@ -1,10 +1,31 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
-public class CursorSwitcher : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+public class CursorSwitcher : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler {
 	public Texture2D cursorTexture;
 	public CursorMode cursorMode = CursorMode.Auto;
 	public Vector2 hotSpot = Vector2.zero;
+
+	private void Awake()
+	{
+		if (GameObject.FindGameObjectWithTag("AudioManager") != null)
+		{
+			if (GameObject.FindGameObjectWithTag("AudioManagerLoaded") != null)
+			{
+				Destroy(GameObject.FindGameObjectWithTag("AudioManager"));
+			}
+			else
+			{
+				GameObject.FindGameObjectWithTag("AudioManager").transform.Find("BgOst").GetComponent<AudioSource>().Play();
+				GameObject.FindGameObjectWithTag("AudioManager").tag = "AudioManagerLoaded";
+			}
+			DontDestroyOnLoad(GameObject.FindGameObjectWithTag("AudioManagerLoaded"));
+		}
+
+	}
 
 	public void OnPointerEnter(PointerEventData eventData) {
 		Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
@@ -12,5 +33,32 @@ public class CursorSwitcher : MonoBehaviour, IPointerEnterHandler, IPointerExitH
 
 	public void OnPointerExit(PointerEventData eventData) {
 		Cursor.SetCursor(null, hotSpot, cursorMode);
+	}
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		Debug.Log(gameObject.tag);
+		if (GameObject.FindGameObjectWithTag("AudioManager") != null)
+		{
+			if (gameObject.CompareTag("UIButton"))
+			{
+				GameObject.FindGameObjectWithTag("AudioManager").transform.Find("ButtonSFX1").GetComponent<AudioSource>().Play();
+			}
+			else
+			{
+				GameObject.FindGameObjectWithTag("AudioManager").transform.Find("ButtonSFX2").GetComponent<AudioSource>().Play();
+			}
+		}
+		else if (GameObject.FindGameObjectWithTag("AudioManagerLoaded") != null)
+		{
+			if (gameObject.CompareTag("UIButton"))
+			{
+				GameObject.FindGameObjectWithTag("AudioManagerLoaded").transform.Find("ButtonSFX1").GetComponent<AudioSource>().Play();
+			}
+			else
+			{
+				GameObject.FindGameObjectWithTag("AudioManagerLoaded").transform.Find("ButtonSFX2").GetComponent<AudioSource>().Play();
+			}
+		}
 	}
 }
